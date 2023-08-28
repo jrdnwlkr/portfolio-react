37f2dce340f4b8689933c6c458141fdc43583849
jrdnwlkr <walker.jordan.m@gmail.com>
2023-08-28 16:52:07 -0400

    added tailwind and Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-lime-400'>
+        </div>
+    )
+}
+
+export default Navbar
